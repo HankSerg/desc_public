736eaf1e06b8ba3dd16e980c373a381a292cd3c5
Sergey N <sergey.norov@yandex.ru>
2021-09-17 12:57:21 +0300

    delete unnecessary

--- chatroom/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from chatroom.views import *
-
-urlpatterns = [
-    path('room/', Rooms.as_view()),
-    path('dialog/', Dialog.as_view()),
-    path('users/', AddUserRoom.as_view()),
-]
